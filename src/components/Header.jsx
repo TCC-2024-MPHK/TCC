eb71b2d2cae832e22dee576508983344f28a786b
TCC-2024-MPHK . <tcc92712@gmail.com>
2024-08-18 18:04:42 +0000

    redefinindo o header e começando a responsividade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,25 +8,44 @@ export default function Header() {
 
   const  handleMenu = () => {
     setOpen((prev)=> !prev);
+    console.log(open);
   }
   return (
     <header className=' bg-verde-header '>
-      <div className=" ">
+      <div className=" mx-auto px-4 sm:px-6 lg:px-8 ">
         <div className="flex items-center justify-between h-16 ">
           <div className="flex items-center">
-            <a className='w-50'href="/"><img  src={logo} alt="logo" /> </a>
+            <a className=''href="/"><img  src={logo} alt="logo" /> </a>
           </div>
 
+          <div className="-mr-2 md:hidden  flex items-center " >
+          
+          <List onClick={(handleMenu)} size={24} color="#fff"/>
+          
+        </div>
           {/* links */}
-          <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-4 text-text-branco font-rem text-xs font-light"  >
-              <a href="/funcionamento">Funcionamento</a>
-              <a href="/sobrenos">Sobre Nós</a>
-              <a href="/referencias">Referências</a>
+          <div className="hidden md:block text-xl">
+            <div className="ml-10 flex items-baseline  space-x-8 text-text-branco font-rem font-light "  >
+              <a href="/sobrenos" className="hover:bg-">Sobre Nós</a>
+              <a href="/referencias" className="">Referências</a>
+              <a href="/funcionamento" className="">Funcionamento</a>
             </div>
           </div>
         </div>
       </div>
+      {/* mobile menu */}
+
+      {open ? (
+        <div className="md:hidden flex bg-bege ">
+          <div className=" pt-2 pb-3 space-y-1 sm:px-3 items-baseline  space-x-8 text-text-preto font-rem font-light ">
+              <a href="/sobrenos" className="block px-3 pb-2">Sobre Nós</a>
+              <a href="/referencias" className="block px-3 pb-2">Referências</a>
+              <a href="/funcionamento" className="block px-3 pb-2">Funcionamento</a>
+          </div>
+
+        </div>
+      ) : null}
+
     </header>
   )
 }
