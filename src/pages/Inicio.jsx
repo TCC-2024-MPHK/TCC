2c6772668a8ad73eed607ab73a6d8c755a7f06b5
TCC-2024-MPHK . <tcc92712@gmail.com>
2024-10-15 15:34:22 +0000

    colocando as fotos do projeto

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,6 +4,11 @@ import Fake_imagem from '/imagem do projeto fake.svg'
 import Quadro from '/quadro.svg'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Protejeto1 from '/foto-pro4.jpeg'
+import Protejeto2 from '/foto-pro2.jpeg'
+import Protejeto3 from '/foto-pro3.jpeg'
+import Protejeto4 from '/foto-pro1.jpeg'
+import Protejeto5 from '/foto-pro5.jpeg'
 
 export default function Inicio() {
   return (
@@ -22,16 +27,16 @@ export default function Inicio() {
                     <p className='pt-9 mb-8 font-aleo text-justify text-xs italic font-semibold lg:pb-10 lg:text-center lg:text-xl '>
                     O cultivo de vegetais proporciona uma série de vantagens para a saúde, tais como, redução do estresse, o consumo regular desses alimentos pode ajudar na prevenção de doenças como câncer, diabetes e obesidade.
                     </p>
-                    <img className='mb-6 md:w-full lg:w-80 lg:h-15 lg:mb-0' src={Fake_imagem} alt="" />
+                    <img className='mb-6 md:w-full lg:w-80 lg:h-15 lg:mb-0' src={Protejeto1} alt="" />
                 </div>
 
 
 
                 <div className='flex items-center mb-28  '  >
-                        <img className=' rounded-md absolute left-20p lg:w-13 lg:h-170px ' src={Quadro} alt="" />   
-                        <img className=' rounded-md absolute left-40p md:left-teste lg:w-13 lg:h-170px' src={Quadro} alt="" />
-                        <img className=' rounded-md absolute right-40p md:right-teste lg:w-13 lg:h-170px' src={Quadro} alt="" />
-                        <img className=' rounded-md absolute right-20p lg:w-13 lg:h-170px' src={Quadro} alt="" />
+                        <img className=' rounded-md absolute left-20p lg:w-13 lg:h-170px max-md:w-20' src={Protejeto3} alt="" />   
+                        <img className=' rounded-md absolute left-40p md:left-teste lg:w-13 lg:h-170px max-md:w-20' src={Protejeto2} alt="" />
+                        <img className=' rounded-md absolute right-40p md:right-teste lg:w-13 lg:h-170px max-md:w-20' src={Protejeto4} alt="" />
+                        <img className=' rounded-md absolute right-20p lg:w-13 lg:h-170px max-md:w-20' src={Protejeto5} alt="" />
                          
                 </div>  
 
